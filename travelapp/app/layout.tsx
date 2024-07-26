4df884200e18573e6842953b0fe69ffc72a593cc
bishalbhatta1028 <bishalbhatta1028@gmail.com>
2024-07-26 20:14:39 +0545

    all file added

--- travelapp/app/layout.tsx
+++ travelapp/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import Index from "./Index";
 
-const inter = Inter({ subsets: ["latin"] });
+
 
 export const metadata: Metadata = {
   title: "Travel",
@@ -11,12 +14,17 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body >{children}</body>
+      <body >
+      <Navbar/>
+   <main className="relative overflow-hidden">      {children}</main>
+ <Footer/>
+
+        </body>
     </html>
   );
 }
